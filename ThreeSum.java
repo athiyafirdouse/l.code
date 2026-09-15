294487b295cc2f52f1ae814f0601b0d49482d1be
Athiya Firdouse <athiyafirdouseu.cse25@veltechmultitech.org>
2026-09-15 22:24:25 +0530

    adding into the file

---
+++ ThreeSum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class ThreeSum
+{
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> result = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            for(int j = i + 1; j < nums.length; j++) {
+
+                for(int k = j + 1; k < nums.length; k++) {
+
+                    if(nums[i] + nums[j] + nums[k] == 0) {
+
+                        List<Integer> temp =
+                            Arrays.asList(nums[i], nums[j], nums[k]);
+
+                        Collections.sort(temp);
+
+                        result.add(temp);
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+}
+    
+    
+
