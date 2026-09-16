9d8a66d8c189607c70e2d14e6b60324954043dac
Athiya Firdouse <athiyafirdouseu.cse25@veltechmultitech.org>
2026-09-16 21:59:41 +0530

    adding into the file

---
+++ palindrome.java
@@ -0,0 +1,29 @@
+ class Palindrome {
+
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        Palindrome p = new Palindrome();
+
+        System.out.println(p.isPalindrome(121));
+        System.out.println(p.isPalindrome(-121));
+        System.out.println(p.isPalindrome(10));
+    }
+}
